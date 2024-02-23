ab19c7524284a2898ddf39f4bee955c45a428b05
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2024-02-23 23:13:18 +0530

    finished

--- src/app/page.js
+++ src/app/page.js
@@ -7,18 +7,63 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <section className="grid grid-cols-12">
-        <div>
-          {" "}
-          <SideNav />{" "}
-        </div>
-        <div className="col-span-8 min-h-screen ">
-          <TaskContent />
-        </div>
-        <div>
-          <Analytics />
-        </div>
-      </section>
+      <div className="hidden  lg:block">
+        <section className=" grid grid-cols-12">
+          <div>
+            {" "}
+            <SideNav />{" "}
+          </div>
+
+          <div className=" col-span-8 min-h-screen ">
+            <TaskContent />
+          </div>
+          <div>
+            <Analytics />
+          </div>
+        </section>
+        <section className=" grid grid-cols-12">
+          <div>
+            {" "}
+            <SideNav />{" "}
+          </div>
+
+          <div className=" col-span-8 min-h-screen ">
+            <TaskContent />
+          </div>
+          <div>
+            <Analytics />
+          </div>
+        </section>
+      </div>
+      {/* mobile */}
+
+      {/* <div className="block  lg:hidden">
+        <section className="flex flex-col justify-center items-center ">
+          <div className="sticky bottom-0">
+            {" "}
+            <SideNav />
+          </div>
+
+          <div className="  ">
+            <Analytics />
+          </div>
+          <div>
+            <TaskContent />
+          </div>
+        </section>
+        <section className=" ">
+          <div>
+            <SideNav />
+          </div>
+
+          <div className=" col-span-8 min-h-screen ">
+            <TaskContent />
+          </div>
+          <div>
+            <Analytics />
+          </div>
+        </section>
+      </div> */}
     </main>
   );
 }
